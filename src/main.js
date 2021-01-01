57a2ba0ed4eb69fac27d1e5154c87ffe44c058a9
changmin <changmin.kim@worksmobile.com>
2021-01-01 18:52:04 +0900

    [NoIssue] 정답으로 제공된 내용 반영

--- src/main.js
+++ src/main.js
@@ -1,51 +1,44 @@
+const loadItems = () => {
+    return fetch('../data/data.json')
+        .then((response) => response.json())
+        .then((json) => json.data)
+}
 
-const getData = (target) => {
-    fetch('../data/data.json')
-        .then((response) => {
-            return response.json()
-        })
-        .then((json) => {
-            const items = document.querySelector("ul")
-            json.data.forEach(item => {
-                newItem =   `<li>
-                                <img class="${item.color} ${item.type} ${item.gender} ${item.size} image" src="${item.image}"/>
-                                <span>${item.gender}, ${item.size} size</span>
-                            </li>` 
-                if(newItem.includes(target)) {
-                    items.innerHTML += newItem
-                }
-                if(!target){
-                    items.innerHTML += newItem
-                }
-            });
-        });
+const distplayItems = (items) => {
+    const container = document.querySelector(".items")
+    console.log(container)
+    container.innerHTML = items.map(item => createHTMLString(item)).join('')
 }
-const reset = ()  => {
-    const items = document.querySelector("ul")
-    items.innerHTML = ""
+
+const createHTMLString = (item) => {
+    return `<li class="item">
+        <img class="${item.color} ${item.type} ${item.gender} ${item.size} image" src="${item.image}"/>
+        <span>${item.gender}, ${item.size} size</span>
+    </li>`
 }
-const tee = () => {
-    reset()
-    getData('tee')
+
+const setEventListeners = (items) => {
+    const logo = document.querySelector('.logo')
+    const buttons = document.querySelector('.menu')
+    logo.addEventListener('click', () => distplayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
 }
-const pants = () => {
-    reset()
-    getData('pants')
+
+const onButtonClick = (event, items) => {
+    const dataset = event.target.dataset
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) {
+        return
+    }
+
+    distplayItems(items.filter(item => item[key] == value))
 }
-const skirt = () => {
-    reset()
-    getData('skirt')
-}
-const blue = () => {
-    reset()
-    getData('blue')
-}
-const yellow = () => {
-    reset()
-    getData('yellow')
-}
-const pink = () => {
-    reset()
-    getData('pink')
-}
-getData()
+
+loadItems()
+    .then(items => {
+        distplayItems(items);
+        setEventListeners(items);
+    })
+    .catch(console.log)
